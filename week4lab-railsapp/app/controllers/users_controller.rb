75ce402b3dc12d542f93d4b5e4ff85d0a9f75bd1
wonjun <jeong.wonjun@gmail.com>
2014-09-25 20:32:26 -0700

    Commit lab code produced from live demo

---
+++ week4lab-railsapp/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
