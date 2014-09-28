16d57bf66ca204e688e25a8b961b2dc9b22a7bc1
Melissa <mpangilinan@ischool.berkeley.edu>
2014-09-28 01:02:20 -0700

    kind of fixed some stuff

--- lab3railsapp/app/controllers/users_controller.rb
+++ lab3railsapp/app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+	@users = User.all
   end
 
   def show
+	@user = User.find(params[:id])
   end
 
-  def update
-  end
 
   def edit
   end
--- lab3railsapp/db/schema.rb
+++ lab3railsapp/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926023145) do
+ActiveRecord::Schema.define(version: 20140928075240) do
 
   create_table "users", force: true do |t|
-    t.integer  "age"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "yearsold"
   end
 
 end
